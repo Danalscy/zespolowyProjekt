8bab35d7437078e31cf56cc216fe4e92a0dbc78d
Jan Zych <jan.zych2@gmail.com>
2018-06-10 02:05:29 +0200

    Added words splitter

---
+++ words_splitter.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+def split_lines_to_words(path_to_file, lines_indexes):
+    list_of_words = []
+
+    img = cv2.imread(path_to_file)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((100,5), np.uint8)
+
+    for upp, down in lines_indexes:
+        words_in_line = split_line_to_words(img[upp:down,:], kernel)
+        for word in words_in_line:
+       	    list_of_words.append(word)
+
+    return list_of_words
+
+def split_line_to_words(line, kernel):
+    list_of_words = []
+
+    ret, thresh = cv2.threshold(line,127,255,cv2.THRESH_BINARY_INV)
+    img_dilation = cv2.dilate(thresh, kernel, iterations=1)
+    im2, ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+ 
+    sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+    for i, ctr in enumerate(sorted_ctrs):
+        x, y, w, h = cv2.boundingRect(ctr)
+        roi = line[y:y+h, x:x+w]
+        list_of_words.append(roi)
+        
+        #cv2.rectangle(image,(x,y),( x + w, y + h ),(0,255,0),2)
+
+    return list_of_words
+
