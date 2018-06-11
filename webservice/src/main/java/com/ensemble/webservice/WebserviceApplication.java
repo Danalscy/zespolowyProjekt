2b9c594df2083d3359f56861eca5fbfe847b162b
Patryk Barton <patryk.barton.pb@gmail.com>
2018-06-11 20:14:56 +0200

    first, not workin

---
+++ webservice/src/main/java/com/ensemble/webservice/WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ensemble.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(WebserviceApplication.class, args);
+    }
+
+}
